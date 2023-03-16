b701e4b534a5ce2b292588a9b1b48b5d63192999
AmeerAllawati <amaab@ucdavis.com>
2023-03-16 14:01:49 +0400

    Added the bot move feature where the player can play against a player or a bot

--- tttBoard.js
+++ tttBoard.js
@@ -1,6 +1,7 @@
 var cells = document.getElementsByClassName('cell');
+var botPlayed = false;
 var mark = 'X';
-var board = ['', '', '', '', '', '', '', '', ''];
+var board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 var winningCombos = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
@@ -8,19 +9,74 @@ var winningCombos = [
 ];
 var gameOver = false;
 
-for(var i = 0; i < cells.length; ++i) {
+function checkWin() {
+    for (var i = 0; i < winningCombos.length; i++) {
+        var combo = winningCombos[i];
+        var a = combo[0], b = combo[1], c = combo[2];
+        if ((board[a] != ' ') && (board[a] == board[b]) && (board[b] == board[c])) {
+            // We have a winner!
+            gameOver = true;
+            alert(board[a] + " wins!");
+            break;
+        }
+    }
+    if (!gameOver && (board.every((val) => val != ' '))) {
+        // It's a draw!
+        gameOver = true;
+        alert("It's a draw!");
+    }
+}
 
-    cells[i].addEventListener('mouseover', function(){
+document.getElementById("myButton").addEventListener("click", function () {
+    const url = "http://localhost:8080/"; // Replace with the URL you want to send the request to
+    const data = { // Replace with the data you want to send in the request body
+        grid: [[board[0], board[1], board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]]],
+        player1: "X",
+        player2: "O"
+    };
+    const options = { // Define the options for the fetch() method
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            var position = data.row*3 + data.col;
+            board[position] = mark;
+            document.getElementById(position).innerHTML = mark;
+            document.getElementById(position).style.fontFamily = 'Comic Sans MS';
+            document.getElementById(position).style.fontSize = '30px';
+            document.getElementById(position).style.textAlign = 'center';
+            document.getElementById(position).style.verticalAlign = 'middle';
+            checkWin();
+            if (mark == 'X') {
+                mark = 'O';
+            } else {
+                mark = 'X';
+            }
+        })
+        .catch(error => console.error(error));
+    
+
+});
+
+for (var i = 0; i < cells.length; ++i) {
+
+    cells[i].addEventListener('mouseover', function () {
         this.style.backgroundColor = 'red';
     });
 
-    cells[i].addEventListener('mouseout', function() {
+    cells[i].addEventListener('mouseout', function () {
         // Change the background color of the cell back to white
         this.style.backgroundColor = 'white';
     });
 
-    cells[i].addEventListener('click', function(){
-        if(!gameOver && this.innerHTML == '') {
+    cells[i].addEventListener('click', function () {
+        if (!gameOver && this.innerHTML == '' && !botPlayed) {
             this.innerHTML = mark;
             this.style.fontFamily = 'Comic Sans MS';
             this.style.fontSize = '30px';
@@ -29,8 +85,8 @@ for(var i = 0; i < cells.length; ++i) {
 
             board[this.id] = mark;
             checkWin();
-
-            if(mark == 'X') {
+        
+            if (mark == 'X') {
                 mark = 'O';
             } else {
                 mark = 'X';
@@ -39,20 +95,4 @@ for(var i = 0; i < cells.length; ++i) {
     });
 }
 
-function checkWin() {
-    for(var i = 0; i < winningCombos.length; i++) {
-        var combo = winningCombos[i];
-        var a = combo[0], b = combo[1], c = combo[2];
-        if((board[a] != '') && (board[a] == board[b]) && (board[b] == board[c])) {
-            // We have a winner!
-            gameOver = true;
-            alert(board[a] + " wins!");
-            break;
-        }
-    }
-    if(!gameOver && (board.every((val) => val != ''))) {
-        // It's a draw!
-        gameOver = true;
-        alert("It's a draw!");
-    }
-}
+
